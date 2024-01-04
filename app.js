ca674dc8b34bb6f69b0ffdc203dfcc4abe5fb76f
LQ <151808233+LQ857@users.noreply.github.com>
2024-01-04 20:03:44 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 dotenv.config();
 
-const PORT=process.env.PORT || 3001;    
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 
 app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
+  console.log(`Server listening on ${PORT}`);
 });
 
-mongoose.connect(process.env.MONGODB_URL).then(() => {
-    console.log('Connected to MongoDB');
-})
-.catch((err) => {
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
     console.log(err);
-});
+  });
